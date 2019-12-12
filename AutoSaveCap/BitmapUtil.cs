6139136d20c7aa813f882f0c778b5d4d49bfcb1c
ma34s <ma34s.sss@gmail.com>
2019-12-12 22:43:33 +0900

    add the logic of Comparing Bitmap refarence from http://tinqwill.blog59.fc2.com/blog-entry-46.html Thank you

---
+++ AutoSaveCap/BitmapUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Drawing;
+
+namespace AutoSaveCap
+{
+    class BitmapUtil
+    {
+        private static byte[] getBmpHash(Bitmap img1)
+        {
+            //ImageConverterで比較
+            ImageConverter ic = new ImageConverter();
+            byte[] byte1 = (byte[])ic.ConvertTo(img1, typeof(byte[]));
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 =
+                new System.Security.Cryptography.MD5CryptoServiceProvider();
+            return md5.ComputeHash(byte1);
+        }
+
+        public static bool Compare(Bitmap img1, Bitmap img2)
+        {
+            //高さが違えばfalse
+            if (img1.Width != img2.Width || img1.Height != img2.Height) return false;
+
+            byte[] hash1 = getBmpHash(img1);
+            byte[] hash2 = getBmpHash(img2);
+            //ハッシュを比較
+            return hash1.SequenceEqual(hash2);
+        }
+    }
+}
+
